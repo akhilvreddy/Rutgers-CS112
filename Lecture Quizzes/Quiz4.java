58700ca6196991c766196b970381d1391ce9671a
Akhil Reddy <avr54@scarletmail.rutgers.edu>
2022-06-16 18:56:50 -0400

    Update Quiz4.java

--- Lecture Quizzes/Quiz4.java
+++ Lecture Quizzes/Quiz4.java
@@ -1,17 +1,16 @@
-	public void insertAt(T item, int index) {
+public void insertAt(T item, int index) {
 		
-		Node<T> current = this.front; 
+   Node<T> current = this.front; 	
+   int counter = 0; 
 		
-		int counter = 0; 
+   while(counter < index){
+      counter++;
+      previous = current; 
+      current = current.next; 
+   }
 		
-		while(counter < index){
-		    counter++;
-		    previous = current; 
-		    current = current.next; 
-		}
-		
-		if(counter==index){
-		    Node<T> insertNode=new Node<T>(item, current.next);
-		    current.setNext(insertNode);    
-		}
-    }
+   if(counter==index){
+      Node<T> insertNode=new Node<T>(item, current.next);
+      current.setNext(insertNode);    
+   }
+}
