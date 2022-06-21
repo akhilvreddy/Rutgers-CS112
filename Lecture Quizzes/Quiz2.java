82e22cef7f69afa8741a8af1eb546f230b8c1cd4
Akhil Reddy <avr54@scarletmail.rutgers.edu>
2022-06-21 19:56:24 -0400

    Update Quiz2.java

--- Lecture Quizzes/Quiz2.java
+++ Lecture Quizzes/Quiz2.java
@@ -1,6 +1,6 @@
 public void deleteLast(){
-        while(this.front.getNext()!=null){
-            this.front = this.front.getNext();
-        } 
-        this.front=null;
-    }
+     while(this.front.getNext()!=null){
+         this.front = this.front.getNext();
+     } 
+     this.front=null;
+}
